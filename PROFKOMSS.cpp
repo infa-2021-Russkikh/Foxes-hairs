9903e9206b4422fe5f6334173800ba1102e01b42
infa-2021-Russkikh <91308403+infa-2021-Russkikh@users.noreply.github.com>
2022-12-18 22:59:09 +0300

    3 rabbits 3 carrots
    I hate github

--- PROFKOMSS.cpp
+++ PROFKOMSS.cpp
@@ -1,7 +1,10 @@
 ﻿#include <iostream>
 #include <cmath>
+#include <stdlib.h>
 #include<Windows.h>
 #include<vector>
+#include <random>
+#include <chrono>
 
 struct Carrot {
     int x;
@@ -10,16 +13,25 @@ struct Carrot {
     void SpawnCarrot() {
         std::cout << "@";
     }
+    void NewCords() {
+        x = rand() % 100;
+        y = rand() % 24;
+    }
+    void DrawCarrot(std::vector<std::string>& fl) {
+        fl[y][x] = '@';
+    }
     Carrot(int x, int y) : x(x), y(y){}
-    bool colision(int a, int b) {
+    /*bool colision(int a, int b) {
         if ((x != a) || (y != b)) {
             return true;
         }
         return false;
-    }
+    }*///я не знаю зачем он тут нужен
 };
 class Creature {
+protected:
     int hunger;
+    int distant;
     int speed;
     int x, y;
     int ostx = 0;
@@ -28,7 +40,7 @@ class Creature {
 public:
 
     bool colision(int a, int b) {
-        if ((x != a) || (y != b)) {
+        if ((x == a) && (y == b)) {
             return true;
         }
         return false;
@@ -124,38 +136,87 @@ public:
 
         }
     }
-    
-    Creature(int x, int y, int sp):x(x), y(y), speed(sp),hunger(5) {}
-   //std::vector<std::string> 
-       void DrawCreature( std::vector<std::string> & fl, int a, int b) {
-       
-               fl[y][x] = '+';
-               GoTo(a, b);
-               fl[y][x] = '*';
-           
+    void DrawCreature(std::vector<std::string>& fl, int a, int b) {
 
-        
+        fl[y][x] = '-';
+        GoTo(a, b);
+        fl[y][x] = '*';
     }
+    
+    Creature() {}
+    Creature(int x, int y, int sp):x(x), y(y), speed(sp),hunger(5),distant(15) {}
+    void ScanAndDrow(std::vector<std::string>& fl, std::vector<Carrot>& fd) {
+        srand(time(NULL));
+        int min = 1000;
         
+        int n=-1;
+        //scans to find closest
+        for (int i = 0; i < fd.size(); i++) {
+            if( ((abs(x - fd[i].x) + abs(y - fd[i].y) < 1000))&&(min> (abs(x - fd[i].x)) + abs(y - fd[i].y))) {
+                n = i;
+                min = abs(x - fd[0].x)+ abs(y - fd[i].y);
+            }
+        }
+        if (n == -1) {
+            DrawCreature(fl, rand() % 99, rand() % 24);
+        }
+        else {
+            DrawCreature(fl, fd[n].x, fd[n].y);
+        }
+
+    }
+    
+        
+};
+class Rabbit :public Creature {
+
+public:
+    //Rabbit():Creature() {}
+    Rabbit(int x, int y, int sp) {
+        Creature(x, y, sp);
+    }
+    void eat() {}
+
 };
 
 
 int main()
 {   
-    Creature Gosha(10, 24, 1);
-
-    Carrot Crosh(1,1);
+    srand(time(NULL));
+    
+    
+    std::vector<Carrot> foods;
+    for (int i = 0; i < 3; i++) {
+        foods.push_back(Carrot(rand() % 100, rand()%24));
+    }
+    std::vector<Rabbit> rabbits;
+    for (int i = 0; i < 3; i++) {
+        rabbits.push_back(Rabbit(10 * i, 20, i+1));
+    }
+    //Rabbit Gosha(10, 24, 3);
     std::vector <std::string> field;
     for (int i = 0; i < 25; i++) {
         field.push_back("----------------------------------------------------------------------------------------------------");
     }
     for (int l = 0; l < 150; l++) {
-        Gosha.DrawCreature(field, Crosh.x, Crosh.y);
-        
+        for (int i = 0; i < 3; i++) {
+            foods[i].DrawCarrot(field);
+        }
+        for (int j = 0; j < rabbits.size(); j++) {
+            for (int i = 0; i < foods.size(); i++) {
+                if (rabbits[j].colision(foods[i].x, foods[i].y)) {
+                    //rabbits[j].eat() на будущее
+                    foods[i].NewCords();
+                }
+            }
+        }
+        for (int p = 0; p < rabbits.size(); p++) {
+            rabbits[p].ScanAndDrow(field, foods);
+        }
         for (int i = 0; i < 25; i++) {
             std::cout << field[i] << '\n';
         }
-        Sleep(100);
+        Sleep(300);
         if (l != 99) {
             system("cls");
         }
